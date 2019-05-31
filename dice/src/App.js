8ab48c42a3d6fbff924c4ded7f37b493341b8a13
JC Smiley <jcsmileyjr@gmail.com>
2019-05-30 20:42:27 -0500

    Initial Title Component and Testing of React-Bootstrap elements
    Delete un-used logo

--- dice/src/App.js
+++ dice/src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Container, Row, Col } from 'react-bootstrap';
 import './App.css';
+
+function Title(props){
+  return(
+    <Container>
+      <Row>
+        <Col>
+        <h1>Let's Play Dice</h1>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Title />
     </div>
   );
 }
